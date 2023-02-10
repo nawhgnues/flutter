113bdefb480733dda27ad0ccc9d2a128410cb9f2
nawhgnues <seungdang123jp@gmail.com>
2023-02-10 22:34:15 +0900

    hello dart

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dart && Flutter
+
+#### study basics of dart and flutter
---
+++ helloworld.dart
@@ -0,0 +1,5 @@
+void main() {
+  //  dart에는 formatter가 자동으로 세미콜론을 붙여주는 기능 없음.
+  // 일부로 세미콜론을 쓰지 않는 경우가 있기 떄문에 
+  print("hello dart!!!");
+}
